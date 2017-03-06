893a2601c810f9012f431db4cdba42a7d58a09d6
johngarcia9110 <johngarcia9110@gmail.com>
2017-03-06 11:24:55 -0500

    webpack debug

---
+++ webpack.dev.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'eval',
+
+  entry: [
+    'webpack-hot-middleware/client',
+    './app/index'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  module: {
+    loaders: [
+      { test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: path.join(__dirname, 'node_modules') },
+      { test: /\.scss?$/,
+        loader: 'style!css!sass',
+        include: path.join(__dirname, 'src', 'styles') },
+      { test: /\.png$/,
+        loader: 'file' },
+      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file'}
+    ]
+  }
+}
